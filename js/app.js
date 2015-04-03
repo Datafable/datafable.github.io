f3ebd22f922777b21a3d5a120fa5edbe4edacbdf
Peter Desmet <peter.desmet.work@gmail.com>
2015-04-03 14:29:58 +0200

    Use data attributes for ScrollSpy

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// Page scrolling feature. Requires jQuery Easing plugin
+// Menu item highlighting is done using Bootstrap ScrollSpy.js, using data attributes
 $(function() {
     $("a.page-scroll").bind("click", function(event) {
         var $anchor = $(this);
         $("html, body").stop().animate({
-            scrollTop: $($anchor.attr("href")).offset().top
-        }, 1500, "easeInOutExpo");
+            scrollTop: ($($anchor.attr("href")).offset().top - 50) // 50px offset for navbar-fixed-top
+        }, 1000, "easeInOutExpo");
         event.preventDefault();
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$("body").scrollspy({
-    target: ".navbar-fixed-top"
-})// Closes the responsive menu on menu item click
+// Closes the responsive menu on menu item click
 $(".navbar-collapse ul li a").click(function() {
     $(".navbar-toggle:visible").click();
 });
